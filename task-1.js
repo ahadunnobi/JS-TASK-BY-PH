76dfba96d2c223921927c512c907022341d3f8a9
ahadunnobi <hacker.ven10@gmail.com>
2026-01-31 23:17:31 +0600

    add multiplication functions with example usage

---
+++ task-1.js
@@ -0,0 +1,7 @@
+// Take four parameters. Multiply the four numbers and then return the result
+function multiply(first, second, third, fourth){
+    return first*second*third*fourth;
+}
+// console.log(multiply(10, 5, 2, 10));
+const multiply2 =(first, second, third, fourth) => first*second*third*fourth;
+console.log(multiply(5, 5, 5, 20));
